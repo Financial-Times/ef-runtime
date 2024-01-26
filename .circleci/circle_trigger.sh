097769e9c1d8fcd73ce35734c99f94593ddb36cc
Omar Bassam <omar.bassam88@gmail.com>
2024-01-26 14:18:55 +0200

    fix: add debug info for CircleCI URL

--- .circleci/circle_trigger.sh
+++ .circleci/circle_trigger.sh
@@ -128,6 +128,7 @@ echo "Triggering pipeline with data:"
 echo -e "  $DATA"
 
 URL="${CIRCLE_API}/v2/project/${REPOSITORY_TYPE}/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/pipeline"
+echo "Callin Circle API URL: $URL"
 HTTP_RESPONSE=$(curl -s -u "${CIRCLE_TOKEN}:" -o response.txt -w "%{http_code}" -X POST --header "Content-Type: application/json" -d "$DATA" "$URL")
 
 if [ "$HTTP_RESPONSE" -ge "200" ] && [ "$HTTP_RESPONSE" -lt "300" ]; then
