feb6636e950f9776c14650639b3ed86b6a31c233
Omar Bassam <omar.bassam88@gmail.com>
2024-03-05 14:55:07 +0200

    fix: `ef-demo-component` and ef-demo-component-ext` local dev

--- examples/ef-demo-component/src/index.js
+++ examples/ef-demo-component/src/index.js
@@ -4,14 +4,24 @@ import './index.css';
 import EFDemoComponent from './EFDemoComponent';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('ef-demo-container'));
-root.render(
-  <React.StrictMode>
-    <EFDemoComponent />
-  </React.StrictMode>
-);
+export async function init() {
+  /* Put your Initialization code here */
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export function mount() {
+  const root = ReactDOM.createRoot(document.getElementById('ef-demo-container'));
+  root.render(
+    <React.StrictMode>
+      <EFDemoComponent />
+    </React.StrictMode>
+  );
+
+  // If you want to start measuring performance in your app, pass a function
+  // to log results (for example: reportWebVitals(console.log))
+  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  reportWebVitals();
+}
+
+export function unmount() {
+  /* Cleanup */
+}
