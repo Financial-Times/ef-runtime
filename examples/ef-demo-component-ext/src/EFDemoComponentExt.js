bcdde90bc3733fe78d6da64bbbb2a225d8a39696
Omar Bassam <omar.bassam88@gmail.com>
2024-03-05 11:42:10 +0200

    feat: add the `ef-demo-component-ext` to this repo

---
+++ examples/ef-demo-component-ext/src/EFDemoComponentExt.js
@@ -0,0 +1,7 @@
+import "./EFDemoComponentExt.css";
+
+function EFDemoComponentExt() {
+    return <h1 className="ef-demo-component-ext">Guess Who's not bundling React<span className="ef-demo-component-ext__devil">😈</span></h1>;
+}
+
+export default EFDemoComponentExt;
